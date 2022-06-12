15bd42072840c1aa6a9e7df47bdef233de56a329
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-12 16:07:31 +0530

    created a stateful widget and assigned it to home

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,51 @@ class MyApp extends StatelessWidget {
       //     ],
       //   ),
       // ),
+      home: SomeWidget(),
+    );
+  }
+}
+
+class SomeWidget extends StatefulWidget {
+  const SomeWidget({Key? key}) : super(key: key);
+  @override
+  State<SomeWidget> createState() => SomeWidgetState();
+}
+
+class SomeWidgetState extends State<SomeWidget> {
+  String btnLabel = "Unclicked";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Basics',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple App'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            print('elevated btn clicked');
+          },
+          child: Text(btnLabel),
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            )
+          ],
+        ),
+      ),
     );
   }
 }

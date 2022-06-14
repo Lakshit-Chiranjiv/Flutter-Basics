b4239f0bd8e950ee9a9e491250393e63fd8373e1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-14 09:20:34 +0530

    created a second page widget

--- lib/main.dart
+++ lib/main.dart
@@ -165,3 +165,18 @@ class SomeWidgetState extends State<SomeWidget> {
     );
   }
 }
+
+
+class SecondPage extends StatelessWidget{
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.red,
+      child: const Text("Second Page"),
+    );
+  }
+}
